41e49372c18f5f07dce26779c5ca7fe7905490ad
RobinJehn <robin.jehn@gmx.de>
2024-02-14 15:42:19 +0000

    Improve scripts

---
+++ scripts/download.sh
@@ -0,0 +1,17 @@
+# Download data
+if [ ! -d "data" ]; then
+    mkdir data
+fi
+
+if [ "$1" = "all" ]; then
+  echo "Downloading all data..."
+  gsutil -m cp -r "gs://mlp-asteroid-data/*" data
+else
+  echo "Downloading only the 30x30 images and lookup files..."
+  gsutil -m cp -r \
+    "gs://mlp-asteroid-data/30x30_images" \
+    "gs://mlp-asteroid-data/mover_images_lookup.csv" \
+    "gs://mlp-asteroid-data/rejected_mover_images_lookup.csv" \
+    data
+fi
+
