7e6ae5c979bc40e20d67039cf2b69e2fadebd1b7
RobinJehn <robin.jehn@gmx.de>
2024-02-16 12:38:32 +0000

    downloading script updated

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,7 +1,5 @@
 # Download data
-if [ ! -d "data" ]; then
-    mkdir data
-fi
+mkdir -p data
 
 if [ "$1" = "all" ]; then
   echo "Downloading all data..."
@@ -12,8 +10,5 @@ elif [ "$1" = "new" ]; then
 else
   echo "Downloading only the 30x30 images and lookup files..."
   gsutil -m cp -r -n "gs://mlp-asteroid-data/30x30_images" data 
-  gsutil -m cp -r \
-    "gs://mlp-asteroid-data/mover_images_lookup.csv" \
-    "gs://mlp-asteroid-data/rejected_mover_images_lookup.csv" \
-    data
+  gsutil -m cp -r "gs://mlp-asteroid-data/csv/*" data
 fi
