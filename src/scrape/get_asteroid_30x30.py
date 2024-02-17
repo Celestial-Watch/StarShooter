b6f9ca8aeaeac86f037d77c6ed4b65a48b44143b
RobinJehn <robin.jehn@gmx.de>
2024-02-17 20:24:02 +0000

    rly fix

--- src/scrape/get_asteroid_30x30.py
+++ src/scrape/get_asteroid_30x30.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
             f.write(
                 "mover_id,file_name,"
                 + ",".join(image_table_labels)
-                + ","
+                + ",pos_"
                 + ",pos_".join(position_table_labels)
                 + "\n"
             )
