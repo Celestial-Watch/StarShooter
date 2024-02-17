2ee502bf6d330d010cc203206129a25c4a895e2e
RobinJehn <robin.jehn@gmx.de>
2024-02-17 13:17:36 +0000

    Model that incorporates metadata

--- src/centered-on-asteroid/model_def.py
+++ src/centered-on-asteroid/model_def.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn as nn
+from typing import Tuple
 
 
 # Convolutional Neural Network
 class CNN(nn.Module):
-    def __init__(self, output_size):
+    def __init__(self, output_size: int):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1)
         self.relu = nn.ReLU()
@@ -12,7 +13,7 @@ class CNN(nn.Module):
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
         self.fc = nn.Linear(32 * 7 * 7, output_size)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.conv1(x)
         x = self.relu(x)
         x = self.maxpool(x)
@@ -24,10 +25,15 @@ class CNN(nn.Module):
         return x
 
 
-# Merge Neural Network
-class MNN(nn.Module):
-    def __init__(self, images_per_sequence, feature_vector_size, image_shape):
-        super(MNN, self).__init__()
+# Convolutional Fusion Network
+class CFN(nn.Module):
+    def __init__(
+        self,
+        images_per_sequence: int,
+        feature_vector_size: int,
+        image_shape: Tuple[int, int],
+    ):
+        super(CFN, self).__init__()
         self.image_shape = image_shape
         self.images_per_sequence = images_per_sequence
         self.feature_vector_size = feature_vector_size
@@ -41,6 +47,44 @@ class MNN(nn.Module):
         images = torch.split(x, self.image_shape[0], dim=2)
         feature_vectors = [self.cnn(image) for image in images]
         feature_vector = torch.cat(feature_vectors, dim=1)
+        x = self.merge(feature_vector)
+        x = torch.sigmoid(x)
+        return x
+
+
+# Meta Convolutional Fusion Network
+class MCFN(nn.Module):
+    def __init__(
+        self,
+        images_per_sequence: int,
+        feature_vector_size: int,
+        image_shape: Tuple[int, int],
+        metadata_size: int,
+    ):
+        super(MCFN, self).__init__()
+        self.image_shape = image_shape
+        self.images_per_sequence = images_per_sequence
+        self.feature_vector_size = feature_vector_size
+        # CNN for the images, ouputs a feature vector
+        self.cnn = CNN(feature_vector_size)
+        # Merge the feature vectors and metadata to a single label
+        self.merge = nn.Linear(
+            images_per_sequence * feature_vector_size + metadata_size, 1
+        )
+
+    def forward(self, x: torch.Tensor, metadata: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            x (torch.Tensor): Concatenated images of shape (n, 1, images_per_sequence * image_shape[0], image_shape[1])
+            metadata (torch.Tensor): Metadata of shape (n, metadata_size)
+        """
+        # The input is the concatenation of the images, split back into individual images
+        images = torch.split(x, self.image_shape[0], dim=2)
+        feature_vectors = [self.cnn(image) for image in images]
+        feature_vectors.append(metadata)
+        feature_vector = torch.cat(feature_vectors, dim=1)
+
+        # Fusion
         x = self.merge(feature_vector)
         x = torch.sigmoid(x)
         return x
