a2dd0aefbac55eee038f26b19a6d4e2186ef46b2
RobinJehn <robin.jehn@gmx.de>
2024-02-26 14:53:27 +0000

    getting stacked images

--- src/centered-on-asteroid/utils.py
+++ src/centered-on-asteroid/utils.py
@@ -25,11 +25,73 @@ def get_dataframe(path_to_csvs: str) -> pd.DataFrame:
     return movers_agg
 
 
+def get_stacked_dataset(
+    movers_agg: pd_typing.DataFrameGroupBy,
+    path_to_images: str,
+    image_shape: tuple = (30, 30),
+) -> Tuple[torch.utils.data.TensorDataset, List[str]]:
+    """
+    Gets a dataset of stacked images and their labels. Each mover is represented as a single 4 x image_shape image.
+
+    Args:
+        movers_agg (pd_typing.DataFrameGroupBy): Grouped DataFrame by mover_id
+        path_to_images (str): Path to images
+        image_shape (tuple, optional): Shape of the images. Defaults to (30, 30).
+    """
+    # Generate input, output pairs
+    x_tensors = []
+    y_hat_tensors = []
+    mover_ids = []
+    for mover_id, group_data in movers_agg:
+        image_tensors = []
+        # Ignore sequences that aren't 4 images long
+        if len(group_data) != 4:
+            print(f"Skipping {mover_id} sequence with length: {len(group_data)}")
+            continue
+
+        for _, row in group_data.iterrows():
+            image_path = path_to_images + row["file_name"]
+            try:
+                # Read image as PIL Image and convert to grayscale
+                image = Image.open(image_path).convert("L")
+            except FileNotFoundError:
+                print(f"Image of {mover_id} not found: {image_path}")
+                break
+
+            # Convert PIL Image to torch.Tensor
+            transform = torchvision.transforms.ToTensor()
+            image_tensor = transform(image)
+
+            # Reshape image tensor to match the expected input shape
+            image_tensor = image_tensor.view(1, 1, *image_shape)
+            image_tensors.append(image_tensor)
+        else:
+            # Loop finished without break
+            # Concatenate over width dimension -> (1, 1, 120, 30)
+            x_tensor = torch.cat(image_tensors, dim=1)
+            x_tensors.append(x_tensor)
+            y_hat_tensors.append(torch.tensor([[group_data["label"].iloc[0]]]))
+            mover_ids.append(mover_id)
+
+    x = torch.concat(x_tensors)
+    y_hat = torch.concat(y_hat_tensors)
+    data_set = torch.utils.data.TensorDataset(x, y_hat)
+    return data_set, mover_ids
+
+
 def get_dataset(
     movers_agg: pd_typing.DataFrameGroupBy,
     path_to_images: str,
     image_shape: tuple = (30, 30),
 ) -> Tuple[torch.utils.data.TensorDataset, List[str]]:
+    """
+    Gets a dataset of combined images and their labels. Each mover is represented as a single 1 x 4*image_shape[0] x image_shape[1] image.
+
+    Args:
+        movers_agg (pd_typing.DataFrameGroupBy): Grouped DataFrame by mover_id
+        path_to_images (str): Path to images
+        image_shape (tuple, optional): Shape of the images. Defaults to (30, 30).
+    """
     # Generate input, output pairs
     x_tensors = []
     y_hat_tensors = []
