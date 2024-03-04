393bf39b2077bf3110dfcc6cd955df489b6c17ea
RobinJehn <robin.jehn@gmx.de>
2024-03-04 15:34:33 +0000

    improve visualize script

--- src/centered-on-asteroid/visualize_with_meta_data.py
+++ src/centered-on-asteroid/visualize_with_meta_data.py
@@ -10,19 +10,13 @@ import model_def
 import matplotlib.pyplot as plt
 import os
 
-model_path = "/Users/robinjehn/Documents/repos/StarShooter/src/centered-on-asteroid/model/end-to-end_20240218_092303/model_20240218_092303_3"
+model_path = "model/end-to-end-max_movement_vector_distance_20240303_222906/model_20240303_222906_1"
+engineered_feature = "max_movement_vector_distance"
 
 # Load model
 images_per_sequence = 4
 feature_vector_size = 10
 image_shape = (30, 30)
-meta_data_size = 1
-
-model = model_def.MCFN(
-    images_per_sequence, feature_vector_size, image_shape, meta_data_size
-)
-model.load_state_dict(torch.load(model_path))
-model.eval()
 
 path_to_data = os.path.abspath("./../../data/")
 real_movers_file = f"{path_to_data}/csv/movers_cond_12_image_meta_data.csv"
@@ -30,29 +24,60 @@ bogus_movers_file = f"{path_to_data}/csv/movers_cond_2_image_meta_data.csv"
 images_folder = f"{path_to_data}/images/centered_on_asteroid/"
 movers_agg = get_dataframe(real_movers_file, bogus_movers_file)
 data_set, mover_ids = get_dataset(movers_agg, images_folder)
+
+# Get engineered features
+movers_agg = movers_agg.filter(lambda x: any(x["mover_id"].isin(mover_ids))).groupby(
+    "mover_id"
+)
 metadata = get_position_tensor(movers_agg)
-# metadata = torch.fill(metadata, 0)
-extra_features = get_engineered_features(metadata)
+extra_features = get_engineered_features(metadata, engineered_feature)
 data_set = CustomDataset(data_set.tensors[0], extra_features, data_set.tensors[1])
 
-wrong_predictions = 0
+meta_data_size = len(extra_features[0])
+model = model_def.MCFN(
+    images_per_sequence, feature_vector_size, image_shape, meta_data_size
+)
+model.load_state_dict(torch.load(model_path))
+model.eval()
+print(f"Model: {model}")
+
+fp = 0
+fn = 0
+plot = False
 for idx, data in enumerate(data_set):
     x_tup, label = data
     x, feature_vector = x_tup
     x = x[None, :, :, :]
     feature_vector = feature_vector[None, :]
     pred = model((x, feature_vector))
-    pred_label = round(pred.item())
+    pred_label = int(pred.item() > 0.5)
     if pred_label != label.item():
-        wrong_predictions += 1
-        fig, axs = plt.subplots(1, 4)
-        images = torch.split(x, 30, dim=2)
-        mover_id = mover_ids[idx]
-        text = f"Prediction for {mover_id}: {pred_label}, Label: {label.item()}"
-        fig.suptitle(text)
-        print(text)
-        for i, image in enumerate(images):
-            axs[i].imshow(image.squeeze().numpy())
-        plt.show()
+        if pred_label == 1:
+            fp += 1
+        else:
+            fn += 1
 
-print(f"{wrong_predictions} wrong predictions out of {len(data_set)} samples.")
+        mover_id = mover_ids[idx]
+        text = f"Prediction for {mover_id}: {pred_label} ({round(pred.item(), 2)}), Label: {label.item()}"
+        print(text)
+        print(f"Feature vector: {feature_vector}\n")
+        if plot:
+            fig, axs = plt.subplots(1, 4)
+            images = torch.split(x, 30, dim=2)
+            fig.suptitle(text)
+            for i, image in enumerate(images):
+                axs[i].imshow(image.squeeze().numpy())
+            plt.show()
+
+precision = 1 - (fp / (fp + fn))
+recall = 1 - (fn / (fp + fn))
+f1_score = 2 * precision * recall / (precision + recall)
+accuracy = (len(data_set) - (fp + fn)) / len(data_set)
+print(f"The model was trained with the {engineered_feature} feature.")
+print(f"{fp} false positives, {fn} false negatives out of {len(data_set)} samples.")
+print(f"False positive rate: {fp / len(data_set)}")
+print(f"False negative rate: {fn / len(data_set)}")
+print(f"Accuracy: {accuracy}")
+print(f"Precision: {precision}")
+print(f"Recall: {recall}")
+print(f"F1 score: {f1_score}")
