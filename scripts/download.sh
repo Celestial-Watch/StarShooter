87550128cd958c08c61b2e5459c4e66763ac2ae6
RobinJehn <robin.jehn@gmx.de>
2024-03-08 18:06:59 +0000

    improve download script

--- scripts/download.sh
+++ scripts/download.sh
@@ -5,10 +5,10 @@ if [ "$1" = "all" ]; then
   echo "Downloading all data..."
   gsutil -m cp -r "gs://mlp-asteroid-data/*" data
 elif [ "$1" = "new" ]; then
-  echo "Downloading all new data..."
-  gsutil -m cp -r -n "gs://mlp-asteroid-data/30x30_images" data
+  echo "Downloading new centered on asteroid images..."
+  gsutil -m cp -r -n "gs://mlp-asteroid-data/images/centered_on_asteroid" data
 else
-  echo "Downloading only the 30x30 images and lookup files..."
-  gsutil -m cp -r -n "gs://mlp-asteroid-data/30x30_images" data 
+  echo "Downloading only the centered on asteroid images and lookup files..."
+  gsutil -m cp -r -n "gs://mlp-asteroid-data/images/centered_on_asteroid" data 
   gsutil -m cp -r "gs://mlp-asteroid-data/csv" data
 fi
