87f47eeab59b43d2b8b8f81ba4bfc41410aa5e80
Tait-A <s2051131@ed.ac.uk>
2024-03-12 14:14:34 +0000

    adding function to be used for multiple classes training

--- src/processing/train_comparison.py
+++ src/processing/train_comparison.py
@@ -2,56 +2,18 @@ import channel_model
 import torch
 import torch.nn as nn
 import os
+import sys
 from typing import Tuple
 from datetime import datetime
 from torch.utils import tensorboard
 import copy
-import os
 from centre_images import get_loaders, get_datasets
 
-
-def train_one_epoch(
-    model: nn.Module,
-    epoch_index: int,
-    logger: tensorboard.writer.SummaryWriter,
-    criterion: nn.modules.loss._Loss,
-    training_loader: torch.utils.data.DataLoader,
-    optimizer: torch.optim.Optimizer,
-    window_size: int = 60,
-):
-    running_loss = 0.0
-    last_loss = 0.0
-
-    # Make sure at least one window is used. Used for inter-epoch reporting
-    window_size = min(window_size, len(training_loader))
-    for i, data in enumerate(training_loader):
-        inputs, labels = data
-        optimizer.zero_grad()  # reset
-        preds = model(inputs)
+sys.path.insert(1, os.path.join(os.path.dirname(__file__), "../centered-on-asteroid"))
+from train import train
 
 
-        # Compute the loss and its gradients
-        loss = criterion(preds, labels.float())
-        loss.backward()
-
-        # Adjust learning weights
-        optimizer.step()
-
-        # Gather data and report
-        running_loss += loss.item()
-        if i % window_size == window_size - 1:
-            last_loss = running_loss / window_size  # loss per batch
-
-            # Log data
-            print("  batch {} loss: {}".format(i + 1, last_loss))
-            batch_number = epoch_index * len(training_loader) + i + 1
-            logger.add_scalar("Loss/train", last_loss, batch_number)
-
-            running_loss = 0.0
-
-    return last_loss
-
-
+# this function is altered to allow the model to be trained with classes for validation
 def get_validation_performance(
     model: nn.Module,
     val_images: torch.Tensor,
@@ -70,78 +32,6 @@ def get_validation_performance(
     return val_loss, val_accuracy
 
 
-def report_performance(
-    logger: tensorboard.writer.SummaryWriter,
-    training_loss: float,
-    val_loss: float,
-    val_accuracy: float,
-    epoch: int,
-) -> None:
-    # Printing
-    print("LOSS train {} valid {}".format(training_loss, val_loss))
-    print("ACCURACY: ", val_accuracy * 100, "%")
-
-    # Loggingn
-    logger.add_scalars(
-        "Training vs. Validation Loss",
-        {"Training": training_loss, "Validation": val_loss},
-        epoch + 1,
-    )
-    logger.add_scalars(
-        "Validation Accuracy",
-        {"Accuracy": val_accuracy},
-        epoch + 1,
-    )
-    logger.flush()
-
-
-def train(
-    model: nn.Module,
-    training_loader: torch.utils.data.DataLoader,
-    val_dataset: torch.utils.data.TensorDataset,
-    criterion: nn.modules.loss._Loss,
-    optimizer: torch.optim.Optimizer,
-    num_epochs: int,
-    experiment_name: str,
-):
-    # Create tensorboard logger
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    run_name = "{}_{}".format(experiment_name, timestamp)
-    logger = tensorboard.writer.SummaryWriter("logs/" + run_name)
-
-    model_folder = "model/{}".format(run_name)
-    os.makedirs(model_folder)
-
-    # Needed to compute validation accuracy
-    val_images, val_labels = val_dataset[:]
-
-    best_val_loss = 1_000_000.0
-    best_model = model
-    for epoch in range(num_epochs):
-        print("EPOCH {}:".format(epoch + 1))
-
-        # Make sure gradient tracking is on, and do a pass over the data
-        model.train()
-        training_loss = train_one_epoch(
-            model, epoch, logger, criterion, training_loader, optimizer
-        )
-
-        val_loss, val_accuracy = get_validation_performance(
-            model, val_images, val_labels, criterion
-        )
-
-        report_performance(logger, training_loss, val_loss, val_accuracy, epoch)
-
-        # Track best performance, and save the model's state
-        if val_loss < best_val_loss:
-            best_val_loss = val_loss
-            best_model = copy.deepcopy(model)
-            model_path = model_folder + "/model_{}_{}".format(timestamp, epoch)
-            torch.save(best_model.state_dict(), model_path)
-
-    return best_model
-
-
 if __name__ == "__main__":
     # Model parameters
     image_shape1 = 30
@@ -154,7 +44,7 @@ if __name__ == "__main__":
 
     # Training parameters
     loss = torch.nn.BCELoss()
-    optimizer1 = torch.optim.Adam(model1.parameters(), lr = 5e-4)
+    optimizer1 = torch.optim.Adam(model1.parameters(), lr=5e-4)
     optimizer2 = torch.optim.Adam(model2.parameters())
     epochs = 100
     batch_size = 20
